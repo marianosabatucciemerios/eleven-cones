c668a70bba615b0e2a52f520e996038297e48e28
marianosabatucciemerios <mariano.sabatucci@emerios.com>
2018-07-26 23:18:25 -0300

    typescript --> javascript la tenes adentro

---
+++ backend-ts/lib/app.ts
@@ -0,0 +1,31 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import { Routes } from "./routes/crmRoutes";
+
+class App {
+
+    public app: express.Application;
+    public mongoUrl: string = 'mongodb://localhost:27017/eleven-cones';
+    public routePrv: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.mongoSetup();
+        this.routePrv.routes(this.app);
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
+    }
+
+}
+
+export default new App().app;
