861f8a07756c3578abf744e43738768759ebb6da
marianosabatucciemerios <mariano.sabatucci@emerios.com>
2018-07-28 19:13:32 -0300

    ssl certification

--- backend-ts/lib/server.ts
+++ backend-ts/lib/server.ts
@@ -1,6 +1,20 @@
 import app from "./app";
+import * as https from 'https';
+import * as fs from 'fs';
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem'),
+    cert: fs.readFileSync('./config/cert.pem')
+};
+
+// -- HTTPS -- //
+https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log('Express secure server listening on port ' + PORT);
 })
+
+// -- HTTP -- //
+// app.listen(PORT, () => {
+//     console.log('Express server listening on port ' + PORT);
+// })
